0e8c3eddf274ea8067234bfd5947ed6a83962854
Karthik A V PHP Developer <karthikav1999@gmail.com>
2025-05-17 20:35:09 +0530

    first commit

---
+++ Sangrv-V3/src/main/java/com/example/sangrv_v3/Sangrv/V3/SangrvV3Application.java
@@ -0,0 +1,13 @@
+package com.example.sangrv_v3.Sangrv.V3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangrvV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangrvV3Application.class, args);
+	}
+
+}
---
+++ Sangrv-V3/src/test/java/com/example/sangrv_v3/Sangrv/V3/SangrvV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangrv_v3.Sangrv.V3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangrvV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
