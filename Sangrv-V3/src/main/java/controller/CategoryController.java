75916abbf0bfa06af94681fa1953c936503658d2
Karthik A V PHP Developer <karthikav1999@gmail.com>
2025-05-17 21:38:13 +0530

    ResponseEntity

--- Sangrv-V3/src/main/java/controller/CategoryController.java
+++ Sangrv-V3/src/main/java/controller/CategoryController.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import model.Category;
 import service.CategoryService;
@@ -34,8 +37,14 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId){
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
+
+        try{
+             String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status,HttpStatus.OK);
+        }catch(ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+       
     }
 }
--- Sangrv-V3/src/main/java/service/CategoryServiceImpl.java
+++ Sangrv-V3/src/main/java/service/CategoryServiceImpl.java
@@ -4,7 +4,9 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import model.Category;
 
@@ -26,9 +28,9 @@ public class CategoryServiceImpl implements CategoryService{
     public String deleteCategory(Long categoryId) {
                 Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-        if (category == null)
-            return "Category not found";
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
+      
 
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully !!";
